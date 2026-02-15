a4ea572808f859a00ddc47f2871b3c50a5987c9a
Joel Chubrega <jchubrega@gmail.com>
2026-02-15 11:51:57 -0300

    add env

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import usersRouter from "./routers/users.router.js";
 import petsRouter from "./routers/pets.router.js";
 import mocksRouter from "./routers/mocks.router.js";
 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
@@ -17,6 +21,6 @@ app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
 app.use("/api/mocks", mocksRouter);
 
-app.listen(8080, () => {
-  console.log("Servidor corriendo en puerto 8080");
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
